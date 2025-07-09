36350fdfcb080c99a6c6cfa4bbe9183835f6d586
sleepyhoon <pokjm99@naver.com>
2025-07-09 15:36:48 +0900

    Feat: Exception 추가

---
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/UserErrorCode.java
@@ -0,0 +1,16 @@
+package io.ejangs.docsa.global.exception.errorcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+    private final String code;
+}
