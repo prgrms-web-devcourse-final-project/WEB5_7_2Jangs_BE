ed2276df2369e406fb2349b189ef5980fd3539b2
sleepyhoon <pokjm99@naver.com>
2025-07-09 09:35:14 +0900

    Chore: 프로젝트 초기 설정

---
+++ src/main/java/io/ejangs/docsa/DocsaApplication.java
@@ -0,0 +1,13 @@
+package io.ejangs.docsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ejangs/docsa/DocsaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ejangs.docsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
