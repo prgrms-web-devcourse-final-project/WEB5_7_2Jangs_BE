44e8e81dc96469845c8f3128706e80eb8ccd8c11
Jeongmin39 <wjdals20309@naver.com>
2025-07-12 02:49:06 +0900

    Feat: 인증 코드 검증 메서드 구현

--- src/main/java/io/ejangs/docsa/domain/auth/app/AuthService.java
+++ src/main/java/io/ejangs/docsa/domain/auth/app/AuthService.java
@@ -1,6 +1,8 @@
 package io.ejangs.docsa.domain.auth.app;
 
+import io.ejangs.docsa.domain.auth.dto.request.CodeCheckRequest;
 import io.ejangs.docsa.domain.auth.dto.request.SignupCodeRequest;
+import io.ejangs.docsa.domain.auth.dto.response.CodeCheckResponse;
 import io.ejangs.docsa.domain.user.dao.UserRepository;
 import io.ejangs.docsa.global.exception.CustomException;
 import io.ejangs.docsa.global.exception.errorcode.AuthErrorCode;
@@ -8,8 +10,10 @@ import jakarta.mail.MessagingException;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -22,18 +26,42 @@ public class AuthService {
     @Value("${auth.signup-code-cache-name}")
     private String signupCacheName;
 
+    @Value("${auth.passcode-cache-name}")
+    private String passcodeCacheName;
+
     public void sendSignupCode(SignupCodeRequest request) throws MessagingException {
 
         if (userRepository.existsByEmail(request.email())) {
             throw new CustomException(AuthErrorCode.DUPLICATE_EMAIL);
         }
 
-        String code = generateVerifyCode();
+        String code = generateCode();
         cacheManager.getCache(signupCacheName).put(request.email(), code);
         mailService.sendSignupAuthCode(request.email(), code);
     }
 
-    private String generateVerifyCode() {
+    public CodeCheckResponse checkCode(CodeCheckRequest request) {
+
+        Cache cache = cacheManager.getCache(signupCacheName);
+        Cache.ValueWrapper cachedValue = cache.get(request.email());
+
+        if (ObjectUtils.isEmpty(cachedValue)) {
+            throw new CustomException(AuthErrorCode.EXPIRED_CODE);
+        }
+
+        String cachedCode = (String) cachedValue.get();
+        if (!cachedCode.equals(request.code())) {
+            throw new CustomException(AuthErrorCode.INVALID_CODE);
+        }
+
+        String passCode = generatePassCode();
+        cacheManager.getCache(passcodeCacheName).put(request.email(), passCode);
+        cache.evict(request.email());
+
+        return new CodeCheckResponse(passCode);
+    }
+
+    private String generateCode() {
         Random random = new Random();
         StringBuilder key = new StringBuilder();
 
@@ -46,5 +74,20 @@ public class AuthService {
             }
         }
         return key.toString();
+    }
+
+    private String generatePassCode() {
+        Random random = new Random();
+        StringBuilder passCode = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            int index = random.nextInt(2);
+
+            switch (index) {
+                case 0 -> passCode.append((char) (random.nextInt(26) + 97)); // a-z
+                case 1 -> passCode.append(random.nextInt(10)); // 0-9
+            }
+        }
+        return passCode.toString();
     }
 }
