d970e4f8a638bd100ca207598680d74b78d5c9f5
Yewon <ky1nonly@gmail.com>
2025-07-11 15:26:58 +0900

    Feat: block의 data, tunes 필드에 Lob 추가

--- src/main/java/io/ejangs/docsa/domain/block/entity/Block.java
+++ src/main/java/io/ejangs/docsa/domain/block/entity/Block.java
@@ -1,15 +1,7 @@
 package io.ejangs.docsa.domain.block.entity;
 
 import io.ejangs.docsa.domain.document.entity.Document;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,8 +23,10 @@ public class Block {
     @Column(nullable = false)
     private String type;
 
+    @Lob
     private String data;
 
+    @Lob
     private String tunes;
 
     @ManyToOne(fetch = FetchType.LAZY)
