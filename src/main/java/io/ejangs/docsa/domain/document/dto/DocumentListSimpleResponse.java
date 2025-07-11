3b9cac166a84b9c8b216271ec4db1b7b4764d8b1
moon <qoanstjdsla@gmail.com>
2025-07-11 14:15:46 +0900

    Refactor: createdAt, updatedAt 응답 OffestDateTime으로 변경

--- src/main/java/io/ejangs/docsa/domain/document/dto/DocumentListSimpleResponse.java
+++ src/main/java/io/ejangs/docsa/domain/document/dto/DocumentListSimpleResponse.java
@@ -1,12 +1,19 @@
 package io.ejangs.docsa.domain.document.dto;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 public record DocumentListSimpleResponse(
         Long id,
         String title,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
+        OffsetDateTime createdAt,
+        OffsetDateTime updatedAt
 ) {
 
+    public DocumentListSimpleResponse(Long id, String title, LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
+        this(id, title, createdAt.atOffset(ZoneOffset.ofHours(9)),
+                updatedAt.atOffset(ZoneOffset.ofHours(9)));
+    }
 }
