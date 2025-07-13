353e8f0b9b56bb7ef7de35bbca1f05326d539fd7
Jeongmin39 <wjdals20309@naver.com>
2025-07-13 23:02:54 +0900

    Fix: conflict 해결

--- src/main/java/io/ejangs/docsa/domain/auth/app/AuthService.java
+++ src/main/java/io/ejangs/docsa/domain/auth/app/AuthService.java
@@ -2,8 +2,8 @@ package io.ejangs.docsa.domain.auth.app;
 
 import io.ejangs.docsa.domain.auth.dto.request.CodeCheckRequest;
 import io.ejangs.docsa.domain.auth.dto.request.SignupCodeRequest;
+import io.ejangs.docsa.domain.user.dao.mysql.UserRepository;
 import io.ejangs.docsa.domain.auth.dto.response.CodeCheckResponse;
-import io.ejangs.docsa.domain.user.dao.UserRepository;
 import io.ejangs.docsa.global.exception.CustomException;
 import io.ejangs.docsa.global.exception.errorcode.AuthErrorCode;
 import jakarta.mail.MessagingException;
