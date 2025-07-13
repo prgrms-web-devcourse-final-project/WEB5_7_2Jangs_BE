1e59fdebc351d0750dfa59e342b0eba8c50b85b3
sleepyhoon <pokjm99@naver.com>
2025-07-14 01:07:53 +0900

    Test: service 테스트 작성

--- src/test/java/io/ejangs/docsa/domain/save/app/SaveServiceUnitTest.java
+++ src/test/java/io/ejangs/docsa/domain/save/app/SaveServiceUnitTest.java
@@ -3,16 +3,23 @@ package io.ejangs.docsa.domain.save.app;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.ejangs.docsa.domain.doc.dao.mysql.DocumentRepository;
 import io.ejangs.docsa.domain.doc.entity.Doc;
+import io.ejangs.docsa.domain.save.dao.SaveRepositoryAdapter;
+import io.ejangs.docsa.domain.save.dao.SaveRepositoryAdapterImpl;
 import io.ejangs.docsa.domain.save.dao.mysql.SaveRepository;
+import io.ejangs.docsa.domain.save.document.SaveContent;
 import io.ejangs.docsa.domain.save.dto.SaveUpdateIdDto;
 import io.ejangs.docsa.domain.save.dto.request.SaveUpdateRequest;
+import io.ejangs.docsa.domain.save.entity.Save;
 import io.ejangs.docsa.domain.user.dao.mysql.UserRepository;
 import io.ejangs.docsa.domain.user.entity.User;
 import io.ejangs.docsa.global.exception.CustomException;
+import io.ejangs.docsa.global.exception.errorcode.SaveErrorCode;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,13 +36,35 @@ class SaveServiceUnitTest {
     @Mock
     private DocumentRepository documentRepository;
     @Mock
-    private SaveRepository saveRepository;
+    private SaveRepositoryAdapterImpl saveRepository;
     @Mock
     private Doc mockDoc;
     @Mock
     private User mockUser;
+    @Mock
+    private Save mockSave;
+    @Mock
+    private SaveContent mockSaveContent;
     @InjectMocks
     private SaveService saveService;
+
+    @Test
+    @DisplayName("성공적인 updateSave")
+    void updateSave_success() {
+        // given
+        SaveUpdateIdDto dto = SaveUpdateIdDto.of(1L, 1L, 1L);
+        SaveUpdateRequest request = new SaveUpdateRequest("content");
+
+        when(userRepository.findById(dto.userId())).thenReturn(Optional.of(mockUser));
+        when(documentRepository.findById(dto.documentId())).thenReturn(Optional.of(mockDoc));
+        when(saveRepository.findSaveById(dto.saveId())).thenReturn(mockSave);
+        when(saveRepository.findSaveContentById(mockSave.getSaveMongoId())).thenReturn(
+                mockSaveContent);
+
+        saveService.updateSave(dto, request);
+
+        verify(saveRepository).updateSave(mockSave, mockSaveContent, "content");
+    }
 
     @Test
     @DisplayName("존재하지 않는 유저 ID로 수정 요청 시 예외가 발생한다")
@@ -74,6 +103,8 @@ class SaveServiceUnitTest {
 
         when(userRepository.findById(dto.userId())).thenReturn(Optional.of(mockUser));
         when(documentRepository.findById(dto.documentId())).thenReturn(Optional.of(mockDoc));
+        when(saveRepository.findSaveById(dto.saveId()))
+                .thenThrow(new CustomException(SaveErrorCode.SAVE_NOT_FOUND));
 
         // when & then
         assertThatThrownBy(() -> saveService.updateSave(dto, request))
