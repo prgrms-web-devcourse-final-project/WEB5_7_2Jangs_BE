6425c863c4002f54aef57b1b7ad4de35b207d801
Jeongmin39 <wjdals20309@naver.com>
2025-07-14 11:42:05 +0900

    Feat: 로그인 DTO 추가

---
+++ src/main/java/io/ejangs/docsa/domain/user/dto/request/UserLoginRequest.java
@@ -0,0 +1,18 @@
+package io.ejangs.docsa.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequest(
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "올바른 이메일 주소를 입력해주세요.")
+        String email,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        String password
+) {
+
+}
---
+++ src/main/java/io/ejangs/docsa/domain/user/dto/response/UserLoginResponse.java
@@ -0,0 +1,8 @@
+package io.ejangs.docsa.domain.user.dto.response;
+
+public record UserLoginResponse(
+
+        Long id
+) {
+
+}
