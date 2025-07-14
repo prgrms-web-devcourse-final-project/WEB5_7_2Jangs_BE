e02e34647f63c8be6e098958a47d51c79d8b5202
Jeongmin39 <wjdals20309@naver.com>
2025-07-14 11:48:22 +0900

    Feat: AuthErrorCode 추가

--- src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
@@ -10,7 +10,8 @@ public enum AuthErrorCode implements ErrorCode {
 
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다.", "DUPLICATE_EMAIL"),
     INVALID_CODE(HttpStatus.BAD_REQUEST, "인증 코드가 일치하지 않습니다.", "INVALID_CODE"),
-    EXPIRED_CODE(HttpStatus.BAD_REQUEST, "인증 코드가 만료되었습니다.", "EXPIRED_CODE");
+    EXPIRED_CODE(HttpStatus.BAD_REQUEST, "인증 코드가 만료되었습니다.", "EXPIRED_CODE"),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 일치하지 않습니다.", "INVALID_CREDENTIALS");
 
     private final HttpStatus status;
     private final String message;
