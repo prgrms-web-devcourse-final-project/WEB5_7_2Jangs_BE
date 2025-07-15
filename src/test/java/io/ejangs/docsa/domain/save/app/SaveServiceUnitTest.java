2ea4a07f2459f7ee19fae3bf710defa26a3f798c
sleepyhoon <pokjm99@naver.com>
2025-07-15 16:29:18 +0900

    Test: test 수정

--- src/test/java/io/ejangs/docsa/domain/save/app/SaveServiceUnitTest.java
+++ src/test/java/io/ejangs/docsa/domain/save/app/SaveServiceUnitTest.java
@@ -2,22 +2,30 @@ package io.ejangs.docsa.domain.save.app;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.ejangs.docsa.domain.branch.entity.Branch;
 import io.ejangs.docsa.domain.doc.dao.mysql.DocRepository;
 import io.ejangs.docsa.domain.doc.entity.Doc;
-import io.ejangs.docsa.domain.save.dao.SaveRepositoryAdapterImpl;
+import io.ejangs.docsa.domain.save.dao.mongodb.SaveContentRepository;
+import io.ejangs.docsa.domain.save.dao.mysql.SaveRepository;
 import io.ejangs.docsa.domain.save.document.SaveContent;
 import io.ejangs.docsa.domain.save.dto.SaveBlock;
 import io.ejangs.docsa.domain.save.dto.SaveUpdateIdDto;
 import io.ejangs.docsa.domain.save.dto.request.SaveUpdateRequest;
+import io.ejangs.docsa.domain.save.dto.response.SaveUpdateResponse;
 import io.ejangs.docsa.domain.save.entity.Save;
+import io.ejangs.docsa.domain.save.util.SaveMapper;
 import io.ejangs.docsa.domain.user.dao.mysql.UserRepository;
 import io.ejangs.docsa.domain.user.entity.User;
 import io.ejangs.docsa.global.exception.CustomException;
+import io.ejangs.docsa.global.exception.errorcode.DocErrorCode;
 import io.ejangs.docsa.global.exception.errorcode.SaveErrorCode;
+import io.ejangs.docsa.global.exception.errorcode.UserErrorCode;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -27,6 +35,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -38,13 +47,15 @@ class SaveServiceUnitTest {
     @Mock
     private DocRepository documentRepository;
     @Mock
-    private SaveRepositoryAdapterImpl saveRepository;
+    private SaveRepository saveRepository;
+    @Mock
+    private SaveContentRepository saveContentRepository;
+    @Mock
+    private Save mockSave;
     @Mock
     private Doc mockDoc;
     @Mock
     private User mockUser;
-    @Mock
-    private Save mockSave;
     @Mock
     private Branch mockBranch;
     @Mock
@@ -73,25 +84,36 @@ class SaveServiceUnitTest {
     @Test
     @DisplayName("성공적인 updateSave")
     void updateSave_success() {
-        when(userRepository.findById(dto.userId())).thenReturn(Optional.of(mockUser));
-        when(documentRepository.findById(dto.documentId())).thenReturn(Optional.of(mockDoc));
-        when(saveRepository.findSaveById(dto.saveId())).thenReturn(mockSave);
-        when(saveRepository.findSaveContentById(mockSave.getSaveMongoId())).thenReturn(
-                mockSaveContent);
+        SaveUpdateResponse expectedResponse = new SaveUpdateResponse(OffsetDateTime.now());
+
+        when(userRepository.existsById(dto.userId())).thenReturn(true);
+        when(documentRepository.existsById(dto.documentId())).thenReturn(true);
+        when(saveRepository.findById(dto.saveId())).thenReturn(Optional.of(mockSave));
+        when(saveContentRepository.findById(mockSave.getSaveMongoId())).thenReturn(
+                Optional.of(mockSaveContent));
 
         when(mockSave.getBranch()).thenReturn(mockBranch);
         when(mockBranch.getDoc()).thenReturn(mockDoc);
         when(mockDoc.getUser()).thenReturn(mockUser);
         when(mockUser.getId()).thenReturn(userId);
+        try (MockedStatic<SaveMapper> mockedMapper = mockStatic(SaveMapper.class)) {
+            mockedMapper.when(() -> SaveMapper.toSaveUpdateResponse(mockSave))
+                    .thenReturn(expectedResponse);
 
-        saveService.updateSave(dto, request);
+            SaveUpdateResponse actualResponse = saveService.updateSave(dto, request);
 
-        verify(saveRepository).updateSave(mockSave, mockSaveContent, data);
+            assertEquals(expectedResponse, actualResponse);
+            verify(saveContentRepository).save(mockSaveContent);
+            verify(saveRepository).save(mockSave);
+        }
     }
 
     @Test
     @DisplayName("존재하지 않는 유저 ID로 수정 요청 시 예외가 발생한다")
     void updateSave_fail_invalidUser() {
+        when(userRepository.existsById(userId)).thenThrow(
+                new CustomException(UserErrorCode.USER_NOT_FOUND));
+
         // when & then
         assertThatThrownBy(() -> saveService.updateSave(dto, request))
                 .isInstanceOf(CustomException.class)
@@ -101,7 +123,9 @@ class SaveServiceUnitTest {
     @Test
     @DisplayName("존재하지 않는 문서 ID로 수정 요청 시 예외가 발생한다")
     void updateSave_fail_invalidDocument() {
-        when(userRepository.findById(dto.userId())).thenReturn(Optional.of(mockUser));
+        when(userRepository.existsById(dto.userId())).thenReturn(true);
+        when(documentRepository.existsById(dto.documentId())).thenThrow(new CustomException(
+                DocErrorCode.DOCUMENT_NOT_FOUND));
 
         // when & then
         assertThatThrownBy(() -> saveService.updateSave(dto, request))
@@ -112,9 +136,9 @@ class SaveServiceUnitTest {
     @Test
     @DisplayName("존재하지 않는 저장 ID로 수정 요청 시 예외가 발생한다")
     void updateSave_fail_invalidSave() {
-        when(userRepository.findById(dto.userId())).thenReturn(Optional.of(mockUser));
-        when(documentRepository.findById(dto.documentId())).thenReturn(Optional.of(mockDoc));
-        when(saveRepository.findSaveById(dto.saveId()))
+        when(userRepository.existsById(dto.userId())).thenReturn(true);
+        when(documentRepository.existsById(dto.documentId())).thenReturn(true);
+        when(saveRepository.findById(dto.saveId()))
                 .thenThrow(new CustomException(SaveErrorCode.SAVE_NOT_FOUND));
 
         // when & then
@@ -140,9 +164,9 @@ class SaveServiceUnitTest {
         Branch branch = Branch.builder().doc(doc).build();
         Save save = Save.builder().branch(branch).build();
 
-        when(userRepository.findById(dto.userId())).thenReturn(Optional.of(requestUser));
-        when(documentRepository.findById(dto.documentId())).thenReturn(Optional.of(doc));
-        when(saveRepository.findSaveById(dto.saveId())).thenReturn(save);
+        when(userRepository.existsById(dto.userId())).thenReturn(true);
+        when(documentRepository.existsById(dto.documentId())).thenReturn(true);
+        when(saveRepository.findById(dto.saveId())).thenReturn(Optional.ofNullable(save));
 
         // when & then
         assertThatThrownBy(() -> saveService.updateSave(dto, request))
