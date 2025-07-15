b50534f4bb0036aa33844953548d2c0c6a436e8d
sleepyhoon <pokjm99@naver.com>
2025-07-15 16:28:48 +0900

    Feat: getSave 구현

--- src/main/java/io/ejangs/docsa/domain/save/app/SaveService.java
+++ src/main/java/io/ejangs/docsa/domain/save/app/SaveService.java
@@ -1,7 +1,9 @@
 package io.ejangs.docsa.domain.save.app;
 
+import com.mongodb.DuplicateKeyException;
 import io.ejangs.docsa.domain.doc.dao.mysql.DocRepository;
-import io.ejangs.docsa.domain.save.dao.SaveRepositoryAdapter;
+import io.ejangs.docsa.domain.save.dao.mongodb.SaveContentRepository;
+import io.ejangs.docsa.domain.save.dao.mysql.SaveRepository;
 import io.ejangs.docsa.domain.save.document.SaveContent;
 import io.ejangs.docsa.domain.save.dto.SaveGetIdDto;
 import io.ejangs.docsa.domain.save.dto.SaveUpdateIdDto;
@@ -15,50 +17,87 @@ import io.ejangs.docsa.global.exception.CustomException;
 import io.ejangs.docsa.global.exception.errorcode.DocErrorCode;
 import io.ejangs.docsa.global.exception.errorcode.SaveErrorCode;
 import io.ejangs.docsa.global.exception.errorcode.UserErrorCode;
+import io.ejangs.docsa.global.util.RenewUpdatedAtHelper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SaveService {
 
     private final DocRepository docRepository;
     private final UserRepository userRepository;
-    private final SaveRepositoryAdapter saveRepository;
+    private final SaveRepository saveRepository;
+    private final SaveContentRepository saveContentRepository;
 
     @Transactional(readOnly = true)
     public SaveGetResponse getSave(SaveGetIdDto dto) {
-        userRepository.findById(dto.userId())
-                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+        checkValidation(dto.userId(), dto.documentId());
 
-        docRepository.findById(dto.documentId())
-                .orElseThrow(() -> new CustomException(DocErrorCode.DOCUMENT_NOT_FOUND));
+        Save findSave = getSaveById(dto.saveId());
 
-        Save findSave = saveRepository.findSaveById(dto.saveId());
-
-        SaveContent saveContent = saveRepository.findSaveContentById(findSave.getSaveMongoId());
+        SaveContent saveContent = getSaveContentById(findSave.getSaveMongoId());
 
         return SaveMapper.toSaveGetResponse(findSave.getUpdatedAt(), saveContent.getContent());
     }
 
     @Transactional
     public SaveUpdateResponse updateSave(SaveUpdateIdDto dto, SaveUpdateRequest request) {
-        userRepository.findById(dto.userId())
-                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+        checkValidation(dto.userId(), dto.documentId());
 
-        docRepository.findById(dto.documentId())
-                .orElseThrow(() -> new CustomException(DocErrorCode.DOCUMENT_NOT_FOUND));
-
-        Save findSave = saveRepository.findSaveById(dto.saveId());
+        Save findSave = getSaveById(dto.saveId());
 
         // SAVE 소유자인지 검사
-        if (!findSave.getBranch().getDoc().getUser().getId().equals(dto.userId())) {
-            throw new CustomException(SaveErrorCode.SAVE_NOT_OWNER);
+        checkSaveOwner(findSave, dto.userId());
+
+        SaveContent saveContent = getSaveContentById(findSave.getSaveMongoId());
+
+        // MySQL 먼저 저장
+        RenewUpdatedAtHelper.touch(findSave);
+        saveRepository.save(findSave);
+
+        // MongoDB 저장
+        saveContent.updateContent(request.content());
+        try {
+            saveContentRepository.save(saveContent);
+        } catch (DuplicateKeyException e) {
+            log.warn("중복 키로 Mongo 저장 실패: {}", e.getMessage());
+            throw new CustomException(SaveErrorCode.SAVE_CREATE_FAIL);
+        } catch (DataAccessException e) {
+            log.error("Mongo 저장 실패: {}", e.getMessage(), e);
+            throw new CustomException(SaveErrorCode.FAILED_TO_SAVE_IN_MONGO);
         }
 
-        SaveContent saveContent = saveRepository.findSaveContentById(findSave.getSaveMongoId());
+        return SaveMapper.toSaveUpdateResponse(findSave);
+    }
 
-        return saveRepository.updateSave(findSave, saveContent, request.content());
+    public Save getSaveById(Long id) {
+        return saveRepository.findById(id)
+                .orElseThrow(() -> new CustomException(SaveErrorCode.SAVE_NOT_FOUND));
+    }
+
+    public SaveContent getSaveContentById(String id) {
+        return saveContentRepository.findById(id)
+                .orElseThrow(() -> new CustomException(SaveErrorCode.SAVE_NOT_FOUND));
+    }
+
+    private void checkValidation(Long userId, Long documentId) {
+        if (!userRepository.existsById(userId)) {
+            throw new CustomException(UserErrorCode.USER_NOT_FOUND);
+        }
+
+        if (!docRepository.existsById(documentId)) {
+            throw new CustomException(DocErrorCode.DOCUMENT_NOT_FOUND);
+        }
+    }
+
+    private void checkSaveOwner(Save save, Long userId) {
+        if (!save.getBranch().getDoc().getUser().getId().equals(userId)) {
+            throw new CustomException(SaveErrorCode.SAVE_NOT_OWNER);
+        }
     }
 }
