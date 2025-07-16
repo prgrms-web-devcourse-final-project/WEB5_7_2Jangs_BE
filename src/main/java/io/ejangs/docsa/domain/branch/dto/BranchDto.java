03952709ebafd456949fd5b7158c097afbe8d7c5
Yewon <ky1nonly@gmail.com>
2025-07-16 11:34:20 +0900

    Fix:누락된  OffsetDateTime 변환 추가

--- src/main/java/io/ejangs/docsa/domain/branch/dto/BranchDto.java
+++ src/main/java/io/ejangs/docsa/domain/branch/dto/BranchDto.java
@@ -1,16 +1,18 @@
 package io.ejangs.docsa.domain.branch.dto;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
-public record BranchDto (
-        Long id,
-        String name,
-        LocalDateTime createdAt,
-        Long fromCommitId,
-        Long rootCommitId,
-        Long leafCommitId,
-        Long saveId
+public record BranchDto(Long id, String name, OffsetDateTime createdAt, Long fromCommitId,
+                        Long rootCommitId, Long leafCommitId, Long saveId
 
-){
+) {
+    public BranchDto(Long id, String name, LocalDateTime createdAt, Long fromCommitId,
+            Long rootCommitId, Long leafCommitId, Long saveId) {
+        this(id, name, createdAt.atOffset(ZoneOffset.ofHours(9)), fromCommitId, rootCommitId,
+                leafCommitId, saveId);
+
+    }
 
 }
