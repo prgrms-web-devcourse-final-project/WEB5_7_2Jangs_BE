29bcd5e3de02ff5ffa983728ef7273631ed42ed1
Jeongmin39 <wjdals20309@naver.com>
2025-07-17 17:09:58 +0900

    Chore: 코드리뷰 반영

--- src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
@@ -16,8 +16,8 @@ public enum AuthErrorCode implements ErrorCode {
     INVALID_SESSION(HttpStatus.UNAUTHORIZED, "세션이 유효하지 않습니다. 다시 로그인해주세요.", "INVALID_SESSION"),
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다.", "AUTHENTICATION_FAILED"),
     ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "이미 가입한 사용자입니다.", "ALREADY_REGISTERED_USER"),
-    USER_NOT_FOUND_FOR_RESET(HttpStatus.BAD_REQUEST, "비밀번호 변경을 위한 사용자를 찾을 수 없습니다.", "USER_NOT_FOUND_FOR_RESET"),
-    CACHE_NOT_FOUND(HttpStatus.NOT_FOUND, "캐시를 찾을 수 없습니다.", "CACHE_NOT_FOUND");
+    CACHE_NOT_FOUND(HttpStatus.NOT_FOUND, "캐시를 찾을 수 없습니다.", "CACHE_NOT_FOUND"),
+    UNSUPPORTED_CODE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 코드 타입입니다.", "UNSUPPORTED_CODE_TYPE");
 
     private final HttpStatus status;
     private final String message;
