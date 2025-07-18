0442a18e6320448203ad2726f61910eeb4bfa51f
sleepyhoon <pokjm99@naver.com>
2025-07-18 15:57:57 +0900

    Refactor: @AuthenticationPrincipal 적용

--- src/main/java/io/ejangs/docsa/domain/branch/api/BranchController.java
+++ src/main/java/io/ejangs/docsa/domain/branch/api/BranchController.java
@@ -5,11 +5,18 @@ import io.ejangs.docsa.domain.branch.dto.request.BranchCreateRequest;
 import io.ejangs.docsa.domain.branch.dto.request.BranchRenameRequest;
 import io.ejangs.docsa.domain.branch.dto.response.BranchCreateResponse;
 import io.ejangs.docsa.domain.branch.dto.response.BranchRenameResponse;
+import io.ejangs.docsa.domain.user.security.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,19 +27,22 @@ public class BranchController {
 
     // 브랜치에 이어서 새로운 저장 생성 or 새로운 브랜치 + 저장 생성
     @PostMapping
-    public ResponseEntity<BranchCreateResponse> createBranchOrSave(@RequestParam Long userId,
+    public ResponseEntity<BranchCreateResponse> createBranchOrSave(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long documentId, @Valid @RequestBody BranchCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(branchService.createBranchOrSave(documentId, request, userId));
+                .body(branchService.createBranchOrSave(documentId, request, userDetails.getId()));
     }
 
     // 브랜치 이름 수정
     @PatchMapping("/{branchId}")
-    public ResponseEntity<BranchRenameResponse> renameBranch(@RequestParam Long userId,
+    public ResponseEntity<BranchRenameResponse> renameBranch(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long documentId, @PathVariable Long branchId,
             @Valid @RequestBody BranchRenameRequest request) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(branchService.renameBranch(documentId, branchId, request.newName(), userId));
+                .body(branchService.renameBranch(documentId, branchId, request.newName(),
+                        userDetails.getId()));
     }
 
 }
--- src/main/java/io/ejangs/docsa/domain/save/api/SaveController.java
+++ src/main/java/io/ejangs/docsa/domain/save/api/SaveController.java
@@ -5,9 +5,11 @@ import io.ejangs.docsa.domain.save.dto.SaveIdentifierDto;
 import io.ejangs.docsa.domain.save.dto.request.SaveUpdateRequest;
 import io.ejangs.docsa.domain.save.dto.response.SaveGetResponse;
 import io.ejangs.docsa.domain.save.dto.response.SaveUpdateResponse;
+import io.ejangs.docsa.domain.user.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,21 +26,23 @@ public class SaveController {
     private final SaveService saveService;
 
     @GetMapping
-    public ResponseEntity<SaveGetResponse> getSave(@RequestParam Long userId,
+    public ResponseEntity<SaveGetResponse> getSave(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long saveId,
             @PathVariable Long documentId) {
         SaveGetResponse response = saveService.getSave(
-                SaveIdentifierDto.of(documentId, saveId, userId));
+                SaveIdentifierDto.of(documentId, saveId, userDetails.getId()));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PutMapping
-    public ResponseEntity<SaveUpdateResponse> updateSave(@RequestParam Long userId,
+    public ResponseEntity<SaveUpdateResponse> updateSave(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long documentId,
             @PathVariable Long saveId,
             @RequestBody SaveUpdateRequest saveUpdateRequest) {
         SaveUpdateResponse response = saveService.updateSave(
-                SaveIdentifierDto.of(documentId, saveId, userId), saveUpdateRequest);
+                SaveIdentifierDto.of(documentId, saveId, userDetails.getId()), saveUpdateRequest);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
