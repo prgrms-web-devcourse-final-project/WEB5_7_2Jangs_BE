e4066b2bb00907b7b974e27df6bd2d1548296d54
moon <qoanstjdsla@gmail.com>
2025-07-22 05:10:35 +0900

    Fix: PageDirectionType.from 예외처리 수정

--- src/main/java/io/ejangs/docsa/domain/doc/model/PageDirectionType.java
+++ src/main/java/io/ejangs/docsa/domain/doc/model/PageDirectionType.java
@@ -1,5 +1,7 @@
 package io.ejangs.docsa.domain.doc.model;
 
+import io.ejangs.docsa.global.exception.CustomException;
+import io.ejangs.docsa.global.exception.errorcode.PageErrorCode;
 import lombok.Getter;
 import org.springframework.data.domain.Sort;
 
@@ -16,11 +18,12 @@ public enum PageDirectionType {
     }
 
     public static PageDirectionType from(String value) {
-        try {
-            return PageDirectionType.valueOf(value.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("지원하지 않는 정렬 방향입니다: " + value);
-        }
+        return switch (value) {
+            case "ASC" -> PageDirectionType.ASC;
+            case "DESC" -> PageDirectionType.DESC;
+            default -> throw new CustomException(PageErrorCode.UNSUPPORTED_DIRECTION_TYPE)
+        };
     }
+
 }
 
