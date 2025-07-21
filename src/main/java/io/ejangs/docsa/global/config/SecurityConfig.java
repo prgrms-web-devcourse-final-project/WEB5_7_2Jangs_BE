3dc0b2574caaee82db5f4a709cf0a6dbe61a3735
Jeongmin39 <wjdals20309@naver.com>
2025-07-21 15:34:33 +0900

    Fix: CORS 허용 도메인 추가

--- src/main/java/io/ejangs/docsa/global/config/SecurityConfig.java
+++ src/main/java/io/ejangs/docsa/global/config/SecurityConfig.java
@@ -84,9 +84,13 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of("http://localhost:3000", "https://web5-7-2jangs-fe.pages.dev/"));
+                List.of("http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://web5-7-2jangs-fe.pages.dev")
+        );
         configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        );
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
