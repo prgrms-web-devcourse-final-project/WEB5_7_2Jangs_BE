9c97ce13abcc53a7b8cc353d0fe923b3d88bb534
Yewon <ky1nonly@gmail.com>
2025-07-23 20:05:26 +0900

    Fix: BranchErrorCode 에러메시지 수정

--- src/main/java/io/ejangs/docsa/global/exception/errorcode/BranchErrorCode.java
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/BranchErrorCode.java
@@ -8,11 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum BranchErrorCode implements ErrorCode {
 
-    BRANCH_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 브랜치를 찾을 수 없습니다.", "BRANCH_NOT_FOUND"),
-    BRANCH_NOT_FOUND_OR_FORBIDDEN(HttpStatus.NOT_FOUND, "해당 브랜치를 찾을 수 없습니다", "BRANCH_NOT_FOUND_OR_FORBIDDEN"),
-    MAIN_BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "메인 브랜치는 삭제할 수 없습니다", "MAIN_BRANCH_DELETE_UNAVAILABLE"),
-    SUB_BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "서브브랜치가 있어 해당 브랜치를 삭제할 수 없습니다", "SUB_BRANCH_DELETE_UNAVAILABLE"),
-    BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "해당 브랜치를 삭제할 수 없습니다", "BRANCH_DELETE_UNAVAILABLE");
+    BRANCH_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 버전을 찾을 수 없습니다.", "BRANCH_NOT_FOUND"),
+    BRANCH_NOT_FOUND_OR_FORBIDDEN(HttpStatus.NOT_FOUND, "해당 버전을 찾을 수 없습니다", "BRANCH_NOT_FOUND_OR_FORBIDDEN"),
+    MAIN_BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "메인 버전을 삭제할 수 없습니다", "MAIN_BRANCH_DELETE_UNAVAILABLE"),
+    SUB_BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "서브 버전이 있어 해당 브랜치를 삭제할 수 없습니다", "SUB_BRANCH_DELETE_UNAVAILABLE"),
+    BRANCH_DELETE_UNAVAILABLE(HttpStatus.BAD_REQUEST, "해당 버전을 삭제할 수 없습니다", "BRANCH_DELETE_UNAVAILABLE");
 
     private final HttpStatus status;
     private final String message;
