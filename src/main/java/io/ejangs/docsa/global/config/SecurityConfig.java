34aa0cfc432d8e63f8dffae1f4910cc2845dc28b
Jeongmin39 <wjdals20309@naver.com>
2025-07-28 15:39:47 +0900

    Chore: SecurityConfig 설정 추가

--- src/main/java/io/ejangs/docsa/global/config/SecurityConfig.java
+++ src/main/java/io/ejangs/docsa/global/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
+                        .requestMatchers("/actuator/prometheus").permitAll()
                         .requestMatchers("/api/auth/session/check").permitAll()
                         .requestMatchers(
                                 "/api/auth/code/signup-email",
