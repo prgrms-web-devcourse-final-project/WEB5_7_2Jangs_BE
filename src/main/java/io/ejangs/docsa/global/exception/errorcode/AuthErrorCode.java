3c7617a9e413a944bb5b8d432836cf6fbb164e69
Jeongmin39 <wjdals20309@naver.com>
2025-07-30 01:55:12 +0900

    Feat: AuthErrorCode 추가

--- src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/AuthErrorCode.java
@@ -18,7 +18,8 @@ public enum AuthErrorCode implements ErrorCode {
     SAME_AS_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 동일한 비밀번호입니다.", "SAME_AS_OLD_PASSWORD"),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "로그인 상태에서는 사용할 수 없습니다.", "ACCESS_DENIED"),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 오류가 발생했습니다. 잠시 후 다시 시도해주세요.", "INTERNAL_ERROR"),
-    UNSUPPORTED_CODE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 코드 타입입니다.", "UNSUPPORTED_CODE_TYPE");
+    UNSUPPORTED_CODE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 코드 타입입니다.", "UNSUPPORTED_CODE_TYPE"),
+    SEND_INTERVAL_TOO_SHORT(HttpStatus.BAD_REQUEST, "인증 코드는 3분 후에 다시 요청할 수 있습니다.", "SEND_INTERVAL_TOO_SHORT");
 
     private final HttpStatus status;
     private final String message;
