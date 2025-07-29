bf14a82f45c12ab0dc15dea692854f954ee17255
2ternal <ddft97@gmail.com>
2025-07-29 14:37:09 +0900

    Comment: 리뷰반영 - ToDo 주석 추가

--- src/main/java/io/ejangs/docsa/domain/commit/app/CommitService.java
+++ src/main/java/io/ejangs/docsa/domain/commit/app/CommitService.java
@@ -154,6 +154,7 @@ public class CommitService {
         try {
             // 문서가 존재하는지 검사
             // 브랜치가 존재하는지 검사
+            // ToDo: API 명세 변경 혹은 유지에 따라 수정이 필요함
             Long baseCommitId = mergeRequest.baseBranchId();
             Long targetCommitId = mergeRequest.targetBranchId();
 
