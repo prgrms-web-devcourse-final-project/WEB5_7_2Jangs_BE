d19b57b6cfff942c0cd86f4827dea63683a5615a
Yewon <ky1nonly@gmail.com>
2025-07-30 17:30:45 +0900

    Docs: 스웨거 수정

--- src/main/java/io/ejangs/docsa/domain/branch/swagger/DeleteBranchDocs.java
+++ src/main/java/io/ejangs/docsa/domain/branch/swagger/DeleteBranchDocs.java
@@ -18,12 +18,12 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Operation(
-        summary = "브랜치 삭제",
+        summary = "버전 삭제",
         description = """
-                브랜치를 삭제합니다. 삭제 조건은 아래와 같습니다:
+                버전를 삭제합니다. 삭제 조건은 아래와 같습니다:
 
-                - 메인 브랜치(`fromCommit == null`)는 삭제 불가
-                - 다른 브랜치가 이 브랜치를 기반(fromCommit)으로 만들어졌다면 삭제 불가
+                - 메인 버전(`fromCommit == null`)는 삭제 불가
+                - 다른 버전이 이 버전를 기반(fromCommit)으로 만들어졌다면 삭제 불가
                 - 블록, 시퀀스, 저장(MongoDB)도 함께 삭제됨
                 """,
         parameters = {
@@ -36,7 +36,7 @@ import java.lang.annotation.Target;
                 ),
                 @Parameter(
                         name = "branchId",
-                        description = "삭제할 브랜치 ID",
+                        description = "삭제할 버전 ID",
                         example = "5",
                         required = true,
                         in = ParameterIn.PATH
@@ -45,32 +45,32 @@ import java.lang.annotation.Target;
         responses = {
                 @ApiResponse(
                         responseCode = "204",
-                        description = "브랜치 삭제 성공"
+                        description = "버전 삭제 성공"
                 ),
                 @ApiResponse(
                         responseCode = "400",
-                        description = "브랜치 삭제 실패 - 삭제 불가능한 브랜치",
+                        description = "버전 삭제 실패 - 삭제 불가능한 버전",
                         content = @Content(
                                 schema = @Schema(implementation = ErrorResponse.class),
                                 mediaType = MediaType.APPLICATION_JSON_VALUE,
                                 examples = {
                                         @ExampleObject(
-                                                name = "메인 브랜치 삭제 시도",
+                                                name = "메인 버전 삭제 시도",
                                                 value = """
                                                         {
                                                           "status": 400,
-                                                          "message": "메인 브랜치는 삭제할 수 없습니다.",
+                                                          "message": "기본 버전은 삭제 또는 수정할 수 없습니다.",
                                                           "error": "MAIN_BRANCH_FIX_UNAVAILABLE"
                                                         }
                                                         """
                                         ),
                                         @ExampleObject(
-                                                name = "파생 브랜치가 존재",
+                                                name = "파생된 버전이 존재",
                                                 value = """
                                                         {
-                                                          "status": 400,
-                                                          "message": "해당 브랜치로부터 파생된 브랜치가 존재합니다.",
-                                                          "error": "SUB_BRANCH_DELETE_UNAVAILABLE"
+                                                            "status": 400,
+                                                            "message": "서브 버전이 있어 해당 버전을 삭제할 수 없습니다.",
+                                                            "error": "SUB_BRANCH_DELETE_UNAVAILABLE"
                                                         }
                                                         """
                                         )
@@ -96,16 +96,16 @@ import java.lang.annotation.Target;
                 ),
                 @ApiResponse(
                         responseCode = "404",
-                        description = "브랜치 삭제 실패 - 문서 또는 브랜치가 존재하지 않음",
+                        description = "버전 삭제 실패 - 문서 또는 버전이 존재하지 않음",
                         content = @Content(
                                 schema = @Schema(implementation = ErrorResponse.class),
                                 mediaType = MediaType.APPLICATION_JSON_VALUE,
                                 examples = @ExampleObject(
                                         value = """
                                                 {
-                                                  "status": 404,
-                                                  "message": "해당 브랜치 또는 문서를 찾을 수 없습니다.",
-                                                  "error": "BRANCH_NOT_FOUND_OR_FORBIDDEN"
+                                                    "status": 404,
+                                                    "message": "해당 버전을 찾을 수 없습니다.",
+                                                    "error": "BRANCH_NOT_FOUND_OR_FORBIDDEN"
                                                 }
                                                 """
                                 )
@@ -113,29 +113,19 @@ import java.lang.annotation.Target;
                 ),
                 @ApiResponse(
                         responseCode = "500",
-                        description = "브랜치 삭제 실패 - MySQL 또는 MongoDB 저장 실패",
+                        description = "버전 삭제 실패 - MySQL 또는 MongoDB 저장 실패",
                         content = @Content(
                                 schema = @Schema(implementation = ErrorResponse.class),
                                 mediaType = MediaType.APPLICATION_JSON_VALUE,
                                 examples = {
                                         @ExampleObject(
-                                                name = "MySQL 저장 실패",
+                                                name = "MySQL 또는 MongoDB에 저장 실패",
                                                 value = """
-                                                        {
-                                                          "status": 500,
-                                                          "message": "저장에 실패했습니다.",
-                                                          "error": "FAIL_TO_SAVE_IN_MYSQL"
-                                                        }
-                                                        """
-                                        ),
-                                        @ExampleObject(
-                                                name = "MongoDB 저장 실패",
-                                                value = """
-                                                        {
-                                                          "status": 500,
-                                                          "message": "저장에 실패했습니다.",
-                                                          "error": "FAILED_TO_SAVE_IN_MONGO"
-                                                        }
+                                                            {
+                                                                "status": 500,
+                                                                "message": "데이터 처리 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+                                                                "error": "DATABASE_ERROR"
+                                                            }
                                                         """
                                         )
                                 }
