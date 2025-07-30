9cc70bf90bd6bf9a8e1b25db311211de132b9802
moon <qoanstjdsla@gmail.com>
2025-07-30 18:06:18 +0900

    Docs: GetDocListDocs 404예외 COMMIT_NOT_FOUND 추가

--- src/main/java/io/ejangs/docsa/domain/doc/swagger/GetDocListDocs.java
+++ src/main/java/io/ejangs/docsa/domain/doc/swagger/GetDocListDocs.java
@@ -194,17 +194,29 @@ import org.springframework.http.MediaType;
                         content = @Content(
                                 mediaType = MediaType.APPLICATION_JSON_VALUE,
                                 schema = @Schema(implementation = ErrorResponse.class),
-                                examples =
-                                @ExampleObject(
-                                        value = """
-                                                    {
-                                                      "status": 404,
-                                                      "message": "해당 저장 데이터를 찾을 수 없습니다.",
-                                                      "error": "SAVE_NOT_FOUND"
-                                                    }
-                                                """,
-                                        name = "저장을 찾을 수 없음."
-                                )
+                                examples = {
+                                        @ExampleObject(
+                                                value = """
+                                                            {
+                                                              "status": 404,
+                                                              "message": "해당 저장 데이터를 찾을 수 없습니다.",
+                                                              "error": "SAVE_NOT_FOUND"
+                                                            }
+                                                        """,
+                                                name = "저장을 찾을 수 없음."
+                                        ),
+                                        @ExampleObject(
+                                                value = """
+                                                            {
+                                                              "status": 404,
+                                                              "message": "해당 기록을 찾을 수 없습니다.",
+                                                              "error": "COMMIT_NOT_FOUND"
+                                                            }
+                                                        """,
+                                                name = "커밋 내용을 찾을 수 없음."
+                                        ),
+
+                                }
                         )
                 ),
                 @ApiResponse(
