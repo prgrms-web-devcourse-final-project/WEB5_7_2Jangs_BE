fb628f1c8e36de84e132dd3059993c670a25fbfb
lunarbae628 <qoanstjdsla@gmail.com>
2025-10-10 01:29:50 +0900

    Fix: Mongo 저장 실패 시 update 롤백 - 정밀도 통일(밀리초) / em.clear 복원

--- src/test/java/io/ejangs/docsa/domain/save/util/SaveServiceUtil.java
+++ src/test/java/io/ejangs/docsa/domain/save/util/SaveServiceUtil.java
@@ -60,4 +60,8 @@ public class SaveServiceUtil {
                 .build();
     }
 
+    public static LocalDateTime trimToMillis(LocalDateTime t) {
+        return t.truncatedTo(ChronoUnit.MILLIS);
+    }
+
 }
