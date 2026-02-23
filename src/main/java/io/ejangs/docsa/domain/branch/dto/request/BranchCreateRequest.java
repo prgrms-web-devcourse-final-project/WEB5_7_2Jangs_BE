0036a54d7af956ca3198d5d6d8297990c6a9be0f
lunarbae628 <qoanstjdsla@gmail.com>
2026-02-24 02:29:00 +0900

    Chore: BranchCreateRequest @NotNull 메시지 오타 수정

--- src/main/java/io/ejangs/docsa/domain/branch/dto/request/BranchCreateRequest.java
+++ src/main/java/io/ejangs/docsa/domain/branch/dto/request/BranchCreateRequest.java
@@ -9,6 +9,6 @@ public record BranchCreateRequest(
         @Size(max = 100, message = "브랜치이름은 100자를 초과 할 수 없습니다.")
         String name,
 
-        @NotNull(message = "이어서 작업할 브랜치를 선택해주세요.")
+        @NotNull(message = "이어서 작업할 커밋을 선택해주세요.")
         Long fromCommitId
 ) {}
