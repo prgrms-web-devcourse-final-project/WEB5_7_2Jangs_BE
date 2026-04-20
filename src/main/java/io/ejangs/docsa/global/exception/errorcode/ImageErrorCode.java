12b72eaf8402ff6c2cfce865af2a80ee6d9e424e
lunarbae628 <qoanstjdsla@gmail.com>
2026-04-21 04:51:41 +0900

    chore: 오타수정

--- src/main/java/io/ejangs/docsa/global/exception/errorcode/ImageErrorCode.java
+++ src/main/java/io/ejangs/docsa/global/exception/errorcode/ImageErrorCode.java
@@ -10,7 +10,7 @@ public enum ImageErrorCode implements ErrorCode {
     INVALID_IMAGE_SIZE(HttpStatus.BAD_REQUEST, "업로드 가능한 최대 용량을 초과했습니다.", "INVALID_IMAGE_SIZE"),
     INVALID_IMAGE_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식입니다.", "INVALID_IMAGE_CONTENT_TYPE"),
     IMAGE_UPLOAD_NOT_COMPLETED(HttpStatus.CONFLICT, "이미지 업로드가 아직 완료되지 않았습니다.", "IMAGE_UPLOAD_NOT_COMPLETED"),
-    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 찾을 . 없습니다.", "IMAGE_NOT_FOUND");
+    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 찾을 수 없습니다.", "IMAGE_NOT_FOUND");
 
 
     private final HttpStatus status;
